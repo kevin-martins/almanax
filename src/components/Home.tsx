c0a29c4678220838fe1da90354d4e16898525ad4
kevin-martins <kev.martinsvieira@gmail.com>
2023-01-30 22:07:20 +0100

    style: center Home cards on all screen types

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,12 +4,12 @@ import { CardProps } from '../models/card'
 
 const Home = () => {
   return (
-    <div className='grid place-items-center'>
-        <div className="absolute flex bottom-1/2 translate-y-1/2 flex-wrap items-center gap-4 max-w-6xl">
-            {cardOptions.map((card: CardProps, i: number) => (
-                <Card key={i + Date.now()} {...card} />
-            ))}
-        </div>
+    <div className='grid place-items-center' style={{ height: "calc(100vh - 76px)" }}>
+      <div className='flex flex-wrap gap-4 max-w-5xl'>
+        {cardOptions.map((card: CardProps, i: number) => (
+          <Card key={i + Date.now()} {...card} />
+        ))}
+      </div>
     </div>
   )
 }
