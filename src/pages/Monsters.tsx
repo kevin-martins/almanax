fc1031c56e5f16dcd7f1ab9b50f8a28d35aa4448
kevin-martins <kev.martinsvieira@gmail.com>
2023-01-30 22:46:40 +0100

    feat: adding new data on screen

--- src/pages/Monsters.tsx
+++ src/pages/Monsters.tsx
@@ -2,11 +2,11 @@ import monsterData from '../api/keepedMonsters.json'
 
 const Monsters = (): JSX.Element => {
     return (
-        <div className='flex flex-wrap gap-4 max-w-6xl mx-auto mt-10'>
+        <div className='flex flex-wrap gap-4 max-w-7xl mx-auto mt-10'>
             {monsterData.map(monster => {
                 return (
-                    <div className='w-80 h-60 bg-white rounded shadow-lg mx-auto'>
-                        <img src={monster.imgUrl} alt={monster.name} />
+                    <div className='w-96 h-60 bg-white rounded shadow-lg mx-auto'>
+                        <img src="https://static.ankama.com/dofus/www/game/monsters/200/670.w200h.png" alt={monster.name} />
                         <p className='text-lg'>{monster.name}</p>
                         <p>{monster.monsterType}</p>
                         <ul>
@@ -22,6 +22,16 @@ const Monsters = (): JSX.Element => {
                         <p>{monster.synergie}</p>
                         <p>{monster.areas}</p>
                         <p>{monster.archi?.name}</p>
+                        <ul>
+                            {monster.spells.map((resistance: any, i: number) => {
+                                const currentElement = Object.keys(resistance)[0]
+                                const min = resistance[currentElement].min
+                                const max = resistance[currentElement].max
+                                return (
+                                    <li>{currentElement}: {max === null ? min : `${min} à ${max}`}</li>
+                                )
+                            })}
+                        </ul>
                     </div>
                 )
             })}
