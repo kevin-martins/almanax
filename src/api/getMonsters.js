db3e7c67065f759f3fa84bc83afe94bd47a32b97
kevin-martins <kev.martinsvieira@gmail.com>
2023-01-30 22:46:25 +0100

    refacto: changes spells key and rerun the generator

--- src/api/getMonsters.js
+++ src/api/getMonsters.js
@@ -15,14 +15,16 @@ const dataMobs = [
         synergie: [],
         spells: [
             { name: "Tranchage Mortel", passif: false, effect: "Os si les pdv de la cible sont inférieur où égaux à 50%." },
-            { name: "À l'abordage !", passif: true, effect: "gagne 1pm par dommage reçu."}
+            { name: "À l'abordage !", passif: true, effect: "gagne 1pm par dommage reçu."},
         ],
     },
     {
         name: "Le Chouque",
         monsterType: MonsterType.Boss,
         synergie: [],
-        spells: [{ name: "Coup de Sabre Maudit", effect: "Applique l'etat 'maudit' au cac qui os si les pdv de la cible sont inférieur où égal à 30%."}]
+        spells: [
+            { name: "Coup de Sabre Maudit", effect: "Applique l'etat 'maudit' au cac qui os si les pdv de la cible sont inférieur où égal à 30%."},
+        ],
     },
     {
         name: "Gloutovore",
@@ -30,7 +32,7 @@ const dataMobs = [
         synergie: ["Trukikol"],
         spells: [
             { name: "Gobage", passif: false, effect: "Os que les enemies seulement mais jusqu'a 2 fois par tour, il attire sans ldv de 3 cases." },
-            { name: "", passif: true, effect: "Lors de tentative de retrait pm: gagne 15% de regen (infini), tenta retrait pa: gagne 1pm (infini)" }
+            { name: "", passif: true, effect: "Lors de tentative de retrait pm: gagne 15% de regen (infini), tenta retrait pa: gagne 1pm (infini)" },
         ]
     },
     {
@@ -38,34 +40,44 @@ const dataMobs = [
         monsterType: MonsterType.Commun,
         archi: { monsterType: MonsterType.Archi, name: "Gloubibou le Gars" },
         synergie: [],
-        spells: [{ name: "Gloutage", effect: "Il os que au cac aliée comme enemies au cac et ça le buff de 200 hp et 2pm infinie, il n'attire pas. Evite de le feed !"}]
+        spells: [
+            { name: "Gloutage", passif: false, effect: "Lançable que au cac et pouvait s'utiliser contre enemies comme aliés, il os la cible pour se buff de 200 hp et 2pm infinie."},
+        ],
     },
     {
         name: "Champa Bleu",
         monsterType: MonsterType.Commun,
         archi: { monsterType: MonsterType.Archi, name: "" },
         synergie: [],
-        spells: "Os au cac seulement. Il a aucun sort de tp, de boost pm ni de sort d'attirance."
+        spells: [
+            { name: "", passif: false, effect: "Os au cac seulement."},
+        ],
     },
     {
         name: "Léolhyène",
         monsterType: MonsterType.Commun,
         archi: { monsterType: MonsterType.Archi, name: "" },
         synergie: [],
-        spells: "Lorsqu'il tape avec le sort 'mort sûre' (lançable uniquement au cac), il applique un etat qui se charge. Au stade 4, vous êtes os. Je ne sais pas si l'etat est indépendant au mob ou si d'autres mobs du même type peuvent booster l'etat."
+        spells: [
+            { name: "Mort Sûre", pasif: false, effect: "Lançable que au cac, il applique un etat qui se charge. Au stade 4, vous êtes os. A déterminer si l'etat est indépendant à chaque Léolhyène ou cumulable entre les Léolhyène. Ce faisait vous pourrez sans doute être os plus rapidement."},
+        ],
     },
     {
         name: "Wa Wobot",
         monsterType: MonsterType.Boss,
         synergie: [],
-        spells: "Les auto-tamponeuses pouses dès que vous êtes poussé dessus. Si vous êtes dans le cas de figure ou 2 auto-tamponeuses vous pousse l'une vers l'autre et vice versa, vous êtes os."
+        spells: [
+            { name: "", passif: true, effect: "Les auto-tamponeuses pouses dès que vous êtes poussé dessus. Si vous êtes dans le cas de figure ou 2 auto-tamponeuses vous pousse l'une vers l'autre et vice versa, vous êtes os." },
+        ],
     },
     {
         name: "Kaskargo",
         monsterType: MonsterType.Commun,
         archi: { monsterType: MonsterType.Archi, name: "" },
         synergie: [""],
-        spells: "pose un glyphe sur la case d'arrivée à chaque fois qu'il se tp et peut coop avec ses enemies. Le glyphe en question fait très très mal environ du 3k/4k dmg"
+        spells: [
+            { name: "", passif: false, effect: "pose un glyphe sur la case d'arrivée à chaque fois qu'il se tp et peut coop avec ses enemies. Le glyphe en question fait très très mal environ du 3k/4k dmg"},
+        ],
     },
     {
         name: "Dragon Cochon",
@@ -79,13 +91,17 @@ const dataMobs = [
     {
         name: "Royalmouth",
         monsterType: MonsterType.Boss,
-        spells: "Os la personne qui lui fait subir des do pou et les personnes qui subissent des do pou contre lui. Attention donc au Boufmouth Légendaire qui peut pousser ! Si il vous pousse contre le boss vous êtes os."
+        spells: [
+            { name: "", passif: true, effect: "Os la personne qui lui fait subir des do pou et les personnes qui subissent des do pou contre lui. Attention donc au Boufmouth Légendaire qui peut pousser ! Si il vous pousse contre le boss vous êtes os." },
+        ],
     },
     {
         name: "Les Tynrils",
         monsterType: MonsterType.Boss,
         synergie: ["Tynril Déconcerté", "Tynril Consterné", "Tynril Ahuri", "Tynril Perfide"],
-        spells: "Peuvent os au cac et se coop entre-eux ! Prevoyez une certaine distance puisqu'il peuvent donc se déplacer de 8 cases (2pm chacun) en tout si ils coop le Tynril le plus proche de vous."
+        spells: [
+            { name: "", passif: false, effect: "Peuvent os au cac et se coop entre-eux ! Prevoyez une certaine distance puisqu'il peuvent donc se déplacer de 8 cases (2pm chacun) en tout si ils coop le Tynril le plus proche de vous." },
+        ],
     },
     {
         name: "Abrakne Sombre",
