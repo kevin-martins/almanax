951102b5eb0017eb03dd0c451dc3e121e33482ee
Kevin <kev.martinsvieira@gmail.com>
2023-01-31 04:02:48 +0100

    style: card style for monster display

--- src/pages/Monsters.tsx
+++ src/pages/Monsters.tsx
@@ -1,15 +1,19 @@
 import monsterData from '../api/keepedMonsters.json'
+import { getPath, monsterTypeToIcon } from '../helpers/helpers'
 
 const Monsters = (): JSX.Element => {
     return (
-        <div className='flex flex-wrap gap-4 max-w-7xl mx-auto mt-10'>
+        <div className='flex flex-wrap gap-10 max-w-7xl mx-auto mt-20 pb-44'>
             {monsterData.map(monster => {
                 return (
-                    <div className='w-96 h-60 bg-white rounded shadow-lg mx-auto'>
-                        <img src="https://static.ankama.com/dofus/www/game/monsters/200/670.w200h.png" alt={monster.name} />
-                        <p className='text-lg'>{monster.name}</p>
-                        <p>{monster.monsterType}</p>
-                        <ul>
+                    <div className='relative w-96 h-60 bg-slate-800 border-2 hover:border-yellow-500 border-gray-900 rounded shadow-lg mx-auto'>
+                        <div className='absolute py-2 px-4 bg-yellow-500 w-max left-1/2 -translate-x-1/2 -top-8 text-center rounded'>
+                            {/* <img src={monsterTypeToIcon(monster.monsterType)} alt={monster.name} className="absolute left-1/2 -translate-x-1/2 -top-3" /> */}
+                            {/* <p>{monster.monsterType}</p> */}
+                            <p className='text-xl font-bold'>{monster.name}</p>
+                        </div>
+                        <img src={getPath(`/assets/monsters/${monster.ankamaId}`)} alt={"pas encore d'image pour: " + monster.name} className='mt-8 text-center mx-auto text-white' />
+                        {/* <ul>
                             {monster.resistances.map((resistance: any, i: number) => {
                                 const currentElement = Object.keys(resistance)[0]
                                 const min = resistance[currentElement].min
@@ -31,7 +35,7 @@ const Monsters = (): JSX.Element => {
                                     <li>{currentElement}: {max === null ? min : `${min} à ${max}`}</li>
                                 )
                             })}
-                        </ul>
+                        </ul> */}
                     </div>
                 )
             })}
