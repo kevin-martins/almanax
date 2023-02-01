b280b4f6e69affb9bbc1ee3173de8645b598d855
kevin-martins <kev.martinsvieira@gmail.com>
2023-02-01 21:52:01 +0100

    fix: remove hover props

--- src/components/MonsterCard.tsx
+++ src/components/MonsterCard.tsx
@@ -4,8 +4,8 @@ const MonsterCard = ({ monster, state }: any) => {
     return (
         <div
             className='relative'
-            onMouseEnter={() => state.setHover(true)}
-            onMouseLeave={() => state.setHover(false)}
+            // onMouseEnter={() => state.setHover(true)}
+            // onMouseLeave={() => state.setHover(false)}
         >
             <div className='absolute py-2 px-4 bg-yellow-500 w-max left-1/2 -translate-x-1/2 -top-12 text-center rounded'>
                 {/* <img src={monsterTypeToIcon(monster.monsterType)} alt={monster.name} className="absolute left-1/2 -translate-x-1/2 -top-3" /> */}
@@ -13,29 +13,6 @@ const MonsterCard = ({ monster, state }: any) => {
                 <p className='text-xl font-bold'>{monster.name}</p>
             </div>
             <img src={getPath(`/assets/monsters/${monster.ankamaId}`)} alt={"pas encore d'image pour: " + monster.name} className={`${state.hover && 'transform scale-90'} mt-8 text-center mx-auto text-white`} />
-            {/* <ul>
-                {monster.resistances.map((resistance: any, i: number) => {
-                    const currentElement = Object.keys(resistance)[0]
-                    const min = resistance[currentElement].min
-                    const max = resistance[currentElement].max
-                    return (
-                        <li>{currentElement}: {max === null ? min : `${min} à ${max}`}</li>
-                    )
-                })}
-            </ul>
-            <p>{monster.synergie}</p>
-            <p>{monster.areas}</p>
-            <p>{monster.archi?.name}</p>
-            <ul>
-                {monster.spells.map((resistance: any, i: number) => {
-                    const currentElement = Object.keys(resistance)[0]
-                    const min = resistance[currentElement].min
-                    const max = resistance[currentElement].max
-                    return (
-                        <li>{currentElement}: {max === null ? min : `${min} à ${max}`}</li>
-                    )
-                })}
-            </ul> */}
         </div>
     )
 }
