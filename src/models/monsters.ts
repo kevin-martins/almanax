3808458f587c4022831196c56c40bc44e27868a3
kevin-martins <kev.martinsvieira@gmail.com>
2023-02-09 14:02:00 +0100

    feat: adding type for a const "Type like" and creating enum Effect

--- src/models/monsters.ts
+++ src/models/monsters.ts
@@ -1,6 +1,69 @@
+export type MonsterTypeProps = "Boss" | "Seulement en donjon" | "Commun" | "Archi-monstre"
+
 export const MonsterTypes = {
     Boss: "Boss",
     DungeonOnly: "Seulement en donjon",
     Commun: "Commun",
     Archi: "Archi-monstre"
+}
+
+export enum Effect {
+    Boost,
+    Heal,
+    BlockHeal,
+    Rall,
+    Os,
+    BigDamages,
+    Range,
+    Cac,
+    Invoke,
+    Push,
+    Attract,
+    Teleport,
+    Coop,
+    Invi,
+    Pass,
+}
+
+export type SpellProps = {
+    effect: Effect,
+    info?: string,
+}
+
+export type SynergieProps = {
+    ankamaId: number,
+    name: string,
+    spells: SpellProps[],
+}
+
+export type StatisticsProps = {
+    PV: ValuesProps,
+    PA?: ValuesProps,
+    PM?: ValuesProps,
+}
+
+export type ResistanceProps = {
+    Terre: ValuesProps,
+    Air: ValuesProps,
+    Feu: ValuesProps,
+    Eau: ValuesProps,
+    Neutre: ValuesProps,
+}
+
+type ValuesProps = {
+    min: number,
+    max: number | null,
+}
+
+export type MonsterProps = {
+    _id: number,
+    ankamaId: number,
+    name: string,
+    level: null,
+    type: string,
+    statistics: StatisticsProps[],
+    resistances: ResistanceProps[],
+    monsterType: MonsterTypeProps,
+    synergie: SynergieProps[],
+    spells: SpellProps[],
 }
