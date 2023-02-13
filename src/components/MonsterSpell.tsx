fe3b663d5b11737e9b14bb15a60c67e422f679f2
Kevin <kev.martinsvieira@gmail.com>
2023-02-13 18:10:11 +0100

    feat: display Effect and its info

--- src/components/MonsterSpell.tsx
+++ src/components/MonsterSpell.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { getFile } from '../helpers/helpers'
-import { SpellProps } from '../models/monsters'
+import { Effect, SpellProps } from '../models/monsters'
 
 const MonsterSpell = (spell: SpellProps): JSX.Element => {
   return (
-    <div>
-        <img src={getFile(`/assets/icons/${spell.effect}`)} />
-      {spell.info}
+    <div className='grid grid-rows-1 grid-flow-col mx-auto w-80'>
+      {/* <img src={getFile(`/assets/icons/${spell.effect}`)} alt={`${Effect[spell.effect]}`} className="ml-auto" /> */}
+      <span className='font-bold text-yellow-500 text-lg'>{`${Effect[spell.effect]}`}</span>
+      <span>: {spell.info}</span>
     </div>
   )
 }
